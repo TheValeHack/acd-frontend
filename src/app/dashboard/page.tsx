060316dd65bcd1f18c8215e2b4a4c036efecec5d
TheValeHack <email.penting.jangan.kepo@gmail.com>
2025-11-27 13:53:50 +0700

    feat: add sort date

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -253,7 +253,19 @@ export default function DashboardPage() {
 
                   {!analysisFetch.loading &&
                     !wellFetch.loading &&
-                    analysisFetch.data?.data?.slice(0,3).map((report: any, index: number) => (
+                    analysisFetch.data?.data?.sort((a: any, b: any) => {
+                        const dateA = new Date(a.created_at).getTime();
+                        const dateB = new Date(b.created_at).getTime();
+
+                        // Jika salah satu atau keduanya NaN, tempatkan objek yang valid di depan
+                        if (isNaN(dateA) || isNaN(dateB)) {
+                            if (isNaN(dateA) && !isNaN(dateB)) return 1; // a tidak valid, pindahkan a ke belakang
+                            if (!isNaN(dateA) && isNaN(dateB)) return -1; // b tidak valid, pindahkan b ke belakang
+                            return 0; // Keduanya tidak valid, jaga urutan
+                        }
+
+                        return dateB - dateA;
+                    }).slice(0,3).map((report: any, index: number) => (
                       <tr
                         key={report.id}
                         className={`hover:bg-gray-50 ${
